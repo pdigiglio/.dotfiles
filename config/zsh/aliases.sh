1d1a1c58c66f3acc42acbf03ed8dd4217070f4ca
Paolo Di Giglio <paolo.digiglio@zuru.com>
2025-02-19 18:30:49 +0100

    zsh: make `ip` output colored

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,3 +1,14 @@
+# Check if an utility exists; i.e. if it can be called from the cmd line. The
+# exit code is success, if the utility exists.
+#
+# @param $1 The utility name
+function utility_exists() {
+    typeset -r utility="$1"
+    which "$1" > /dev/null
+}
+
 alias ls="ls --color=auto "
 alias grep="grep --color=auto "
 alias vim="nvim "
+
+utility_exists ip && alias ip='ip --color=auto '
