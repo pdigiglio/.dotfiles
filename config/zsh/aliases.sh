321e8f750317584880ab181f62fb1c281c46ea7a
Paolo Di Giglio <paolo.digiglio@zuru.com>
2025-02-21 23:48:34 +0100

    zsh: add `y` function (yazi + change dir on 'q')

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -12,3 +12,15 @@ alias grep="grep --color=auto "
 alias vim="nvim "
 
 utility_exists ip && alias ip='ip --color=auto '
+
+if utility_exists yazi
+then
+    function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            builtin cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+    }
+fi
