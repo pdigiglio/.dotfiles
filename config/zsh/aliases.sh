374d920fc7aaba38bbb90fb9233587eb4ee37d14
Paolo Di Giglio <paolo.digiglio@zuru.com>
2025-02-22 22:36:34 +0100

    zsh: add alias for sudo, change alias for ls

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -7,9 +7,13 @@ function utility_exists() {
     which "$1" > /dev/null
 }
 
-alias ls="ls --color=auto "
+# Make sure user-defined aliases work when using sudo.
+# See: https://stackoverflow.com/q/37209913
+alias sudo='sudo '
+alias ls='ls --color=auto --hyperlink=auto '
 alias grep="grep --color=auto "
-alias vim="nvim "
+#alias vim="nvim "
+
 
 utility_exists ip && alias ip='ip --color=auto '
 
