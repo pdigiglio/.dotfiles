f72c07000c6427ad72951592f624aa6e40934d15
Paolo Di Giglio <p.digiglio91@gmail.com>
2025-07-14 19:50:27 +0200

    zsh: download, source, and configure 'fzf-git.sh'

--- config/zsh/fzf.zsh
+++ config/zsh/fzf.zsh
@@ -4,9 +4,29 @@
 utility_exists fzf || return
 
 # Set up fzf key bindings and fuzzy completion
-source <(fzf --zsh)
+. <(fzf --zsh)
 
 # Make history search with '/' and '?' interactive. Unlike the built-in binds,
 # they'll search in the same direaction.
 bindkey -M vicmd '/' fzf-history-widget
 bindkey -M vicmd '?' fzf-history-widget
+
+# Check if the fzf/git integration is available.
+if [ -f "$zdot_dir/fzf-git.sh" ]
+then
+    # Source the file.
+    . "$zdot_dir/fzf-git.sh"
+
+    # As I have KEYTIMEOUT=1, the default keybindings (i.e. <C-g><C-b> for
+    # branches) won't work. I remap them here:
+    bindkey '\eb' fzf-git-branches-widget
+    bindkey '\ee' fzf-git-each_ref-widget
+    bindkey '\ef' fzf-git-files-widget
+    bindkey '\eh' fzf-git-hashes-widget
+    bindkey '\el' fzf-git-lreflogs-widget
+    bindkey '\er' fzf-git-remotes-widget
+    bindkey '\es' fzf-git-stashes-widget
+    bindkey '\et' fzf-git-tags-widget
+    bindkey '\ew' fzf-git-worktrees-widget
+    # NOTE: some of these mappings conflict with my media keys (e.g. <A-l>).
+fi
