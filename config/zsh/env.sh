6e47aa46273b50433645ae9cfc5fc7c96da26011
Paolo Di Giglio <p.digiglio91@gmail.com>
2025-09-05 18:22:11 +0200

    zsh: export CLANGD_FLAGS env variable

--- config/zsh/env.sh
+++ config/zsh/env.sh
@@ -13,5 +13,16 @@ export PATH
 # See: https://github.com/python/cpython/issues/118840
 export PYTHON_BASIC_REPL=1
 
-export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
-export EDITRC="${XDG_CONFIG_HOME}/editline/editrc"
+export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
+export EDITRC="$XDG_CONFIG_HOME/editline/editrc"
+
+# -- clangd
+#
+# The following flags are taken from: 
+# https://www.reddit.com/r/cpp/comments/ucz051/how_to_speed_up_clangd_on_big_project
+#
+# NOTE:
+#  - Should they be comma separated or is it fine?
+#  - How to check if clangd is using them?
+#
+export CLANGD_FLAGS="$CLANGD_FLAGS -j=8 --malloc-trim --background-index --pch-storage=memory"
