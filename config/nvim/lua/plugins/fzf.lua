a90d7116439a623c237dcfe99333c62b3487066a
Paolo Di Giglio <p.digiglio91@gmail.com>
2025-11-25 16:01:56 +0100

    nvim: fzf-lua: add more keybindings

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -8,6 +8,17 @@ local fzf_lua_config = function()
     vim.keymap.set('n', '<Leader>fh', fzf_lua.helptags)
     vim.keymap.set('n', '<Leader>fm', fzf_lua.marks)
 
+    vim.keymap.set('n', '<Leader>fq', fzf_lua.quickfix)
+    vim.keymap.set('n', '<Leader>fQ', fzf_lua.quickfix_stack)
+    vim.keymap.set('n', '<Leader>fl', fzf_lua.loclist)
+    vim.keymap.set('n', '<Leader>fL', fzf_lua.loclist_stack)
+
+    vim.keymap.set('n', '<Leader>fr', fzf_lua.lsp_references)
+    vim.keymap.set('n', '<Leader>fs', fzf_lua.lsp_document_symbols)
+    vim.keymap.set('n', '<Leader>fS', fzf_lua.lsp_workspace_symbols)
+    vim.keymap.set('n', '<Leader>fd', fzf_lua.lsp_document_diagnostics)
+    vim.keymap.set('n', '<Leader>fD', fzf_lua.lsp_workspace_diagnostics)
+
     vim.keymap.set('n', '<Leader>f*', fzf_lua.grep_cword)
     vim.keymap.set('v', '<Leader>f*', fzf_lua.grep_visual)
     vim.keymap.set('n', '<Leader>fg', -- fzf_lua.live_grep)
