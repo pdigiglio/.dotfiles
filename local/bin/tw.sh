a2d87f3529f6962faacb0f0aab744c06a3209621
Paolo Di Giglio <p.digiglio91@gmail.com>
2026-03-03 20:45:12 +0100

    tw: make she-bang more flexible

--- local/bin/tw.sh
+++ local/bin/tw.sh
@@ -1,24 +1,24 @@
-#!/bin/zsh
+#!/bin/env bash
 
 # A small script to `watch` my task list. I need to override some "rc" values
 # to get a satisfying report.
 #
-# Run with:
+# Run with, e.g.:
 #
 # watch --color --no-title sh tw.sh
 
 function tw {
-    typeset -r lines=$(tput lines)
-    typeset -r cols=$(tput cols)
-    task \
-        rc.verbose:0 \
-        rc.limit:page \
-        rc.defaultwidth:$cols \
-        rc.defaultheight:$lines \
-        long
+	typeset -r lines=$(tput lines)
+	typeset -r cols=$(tput cols)
+	task \
+		rc.verbose:0 \
+		rc.limit:page \
+		rc.defaultwidth:$cols \
+		rc.defaultheight:$lines \
+		long
 
-        # rc._forcecolor:on \
-        # rc.detection:off \
+	# rc._forcecolor:on \
+	# rc.detection:off \
 }
 
 tw
